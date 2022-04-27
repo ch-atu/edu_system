5a3ad226455737095768aaaa526aad7d0a2e1ae9
“chatu” <634930192@qq.com>
2022-04-27 13:08:07 +0800

    更新相关配置

--- edu-admin/src/main.js
+++ edu-admin/src/main.js
@@ -9,8 +9,8 @@ import axios from 'axios'
 import Print from './utils/vue-print-nb/src'
 Vue.use(Print)
 // 配置请求的根路径
-axios.defaults.baseURL = 'http://119.29.139.149:8000/'
-// axios.defaults.baseURL = 'http://127.0.0.1:8000/'
+// axios.defaults.baseURL = 'http://119.29.139.149:8000/'
+axios.defaults.baseURL = 'http://127.0.0.1:8000/'
 // axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
 // 请求拦截器
 axios.interceptors.request.use(config=>{
